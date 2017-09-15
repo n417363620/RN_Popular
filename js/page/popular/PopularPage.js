656276ca0f14fbb546dd3e6c4b07a6ea0af33283
niuyunyang <yunyang.niu@i-jia.net>
2017-09-15 21:26:45 +0800

    调整了顶部导航自定义添加标签呢的功能，但是在添加多个标签后无法做到滚动，导致标签页挤压的问题

--- js/page/popular/PopularPage.js
+++ js/page/popular/PopularPage.js
@@ -28,6 +28,7 @@ import FavoritePage from "../favorite/FavoritePage";
 const URL='https://api.github.com/search/repositories?q=';
 const QUERY_STR='&sort=stars'
 var ScrollableTabView = require('react-native-scrollable-tab-view');
+const KEYS=['ALL','IOS','React-Native']
 export default class PopularPage extends Component {
     // 构造
     constructor(props) {
@@ -36,9 +37,7 @@ export default class PopularPage extends Component {
         this.state = {
         };
     }
-
     render() {
-
         return (
             <View style={{flex:1}}>
                 <NavigationBar
@@ -128,71 +127,37 @@ const styles = StyleSheet.create({
     }
 });
 //todo 底部按钮注册位置
-const PopularPageTabs = TabNavigator(
-    {
-        PopularTab: {
-            screen: PopularPageTab,
-            path: '/',
-            navigationOptions: {
-                tabBarLabel: '最热',
-            },
-        },
-        TrendingTab: {
-            screen: PopularPageTab,
-            path: '/',
-            navigationOptions: {
-                tabBarLabel: '趋势',
-            },
-        },
-        FavoriteTab: {
-            screen: PopularPageTab,
-            path: '/',
-            navigationOptions: {
-                tabBarLabel: '收藏',
-            },
-        },
-        MineTab: {
-            screen: PopularPageTab,
-            path: '/',
-            navigationOptions: {
-                tabBarLabel: '我的',
-            },
-        },
-    },
-    {
-        tabBarPosition: 'top', // 设置tabbar的位置，iOS默认在底部，安卓默认在顶部。（属性值：'top'，'bottom')
-        swipeEnabled: true, // 是否允许在标签之间进行滑动。
-        animationEnabled: false, // 是否在更改标签时显示动画。
-        lazy: true, // 是否根据需要懒惰呈现标签，而不是提前制作，意思是在app打开的时候将底部标签栏全部加载，默认false,推荐改成true哦。
-        // initialRouteName: 'MineTab', // 设置默认的页面组件
-        backBehavior: 'none', // 按 back 键是否跳转到第一个Tab(首页)， none 为不跳转
-        tabBarOptions: {
-            // iOS属性
-            // 因为第二个tabbar是在页面中创建的，所以前景色的设置对其无效，当然也可以通过设置tintColor使其生效
-            activeTintColor: 'mintcream', // label和icon的前景色 活跃状态下（选中）。
-            inactiveTintColor: 'white', // label和icon的前景色 不活跃状态下(未选中)。
-            style:{height:40,backgroundColor:'#912CEE'},//整个bar的样式
-            indicatorStyle:{backgroundColor:'#ddd',height:2},
-            //activeBackgroundColor:'white', //label和icon的背景色 活跃状态下（选中） 。
-            //  inactiveBackgroundColor:'white', // label和icon的背景色 不活跃状态下（未选中）。
-            showLabel: true, // 是否显示label，默认开启。
-            // style:{}, // tabbar的样式。
-            // labelStyle:{}, //label的样式。
-            // 安卓属性
-            // activeTintColor:'', // label和icon的前景色 活跃状态下（选中） 。
-            // inactiveTintColor:'', // label和icon的前景色 不活跃状态下(未选中)。
-            showIcon: false, // 是否显示图标，默认关闭。
-            // showLabel:true, //是否显示label，默认开启。
-            // style:{}, // tabbar的样式。
-            // labelStyle:{}, // label的样式。
-            //  upperCaseLabel: false, // 是否使标签大写，默认为true。
-            // pressColor:'', // material涟漪效果的颜色（安卓版本需要大于5.0）。
-            // pressOpacity:'', // 按压标签的透明度变化（安卓版本需要小于5.0）。
-            // scrollEnabled:false, // 是否启用可滚动选项卡。
-            // tabStyle:{}, // tab的样式。
-            // indicatorStyle:{}, // 标签指示器的样式对象（选项卡底部的行）。安卓底部会多出一条线，可以将height设置为0来暂时解决这个问题。
-            // labelStyle:{}, // label的样式。
-            // iconStyle:{}, // 图标的样式。
-        }}
-);
 
+function builderTabPage() {
+    let RouteConfigs={}
+    KEYS.forEach((value,key,array)=>{
+        let item = {
+                screen: PopularPageTab,
+                path: '/',
+                navigationOptions: {
+                    tabBarLabel: value,
+                }
+            }
+
+        RouteConfigs[value] = item;
+    })
+    
+   return RouteConfigs
+}
+let TabNavigatorConfig={
+    tabBarPosition: 'top', // 设置tabbar的位置，iOS默认在底部，安卓默认在顶部。（属性值：'top'，'bottom')
+    swipeEnabled: true, // 是否允许在标签之间进行滑动。
+    animationEnabled: false, // 是否在更改标签时显示动画。
+    lazy: true, // 是否根据需要懒惰呈现标签，而不是提前制作，意思是在app打开的时候将底部标签栏全部加载，默认false,推荐改成true哦。
+    // initialRouteName: 'MineTab', // 设置默认的页面组件
+    backBehavior: 'none', // 按 back 键是否跳转到第一个Tab(首页)， none 为不跳转
+    tabBarOptions: {
+        activeTintColor: 'mintcream', // label和icon的前景色 活跃状态下（选中）。
+        inactiveTintColor: 'white', // label和icon的前景色 不活跃状态下(未选中)。
+        style: {height: 40, backgroundColor: '#912CEE'},//整个bar的样式
+        indicatorStyle: {backgroundColor: '#ddd', height: 2},
+        showLabel: true, // 是否显示label，默认开启。
+        showIcon: false,
+    }
+};
+const PopularPageTabs = TabNavigator(builderTabPage(),TabNavigatorConfig);
